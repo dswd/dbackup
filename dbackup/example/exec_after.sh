5ac801488232c8c4442b2769ff4acd4f928abf4a
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2014-09-03 09:41:57 +0200

    Added examples for exec_* scripts

---
+++ dbackup/example/exec_after.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#THIS FILE NEEDS TO BE EXECUTABLE
+
+# Execute some commands after running backup
---
+++ dbackup/example/exec_before.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#THIS FILE NEEDS TO BE EXECUTABLE
+
+# Execute some commands before running backup
